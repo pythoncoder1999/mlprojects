e584ac56a3afb840a35149a142f158e58784e468
pythoncoder1999 <mrs466@scarletmail.rutgers.edu>
2024-08-15 12:13:44 -0400

    data_ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,39 @@
+#read data from source
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass #used to create class variables
+
+@dataclass 
+class DataIngestionConfig: #good to store only data
+    train_data_path: str=os.path.join('artifacts', "train.csv")
+    test_data_path: str=os.path.join('artifacts', "test.csv")
+    raw_data_path: str=os.path.join('artifacts', "raw.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv('C:/mlprojects/preparation/data/StudentsPerformance.csv')
+            logging.info("Read dataset as dataframe")
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header = True)
+            logging.info("Saved dataset as in raw data path")
+            train_set,test_set =train_test_split(df, test_size=0.25,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header = True)
+            logging.info("Saved datasets as for train and test data path")
+
+            return (self.ingestion_config.train_data_path, self.ingestion_config.train_data_path)
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
