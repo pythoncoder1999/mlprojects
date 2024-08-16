5ead8ff2f2c1d3ac085905d3610874c632d5f592
pythoncoder1999 <mrs466@scarletmail.rutgers.edu>
2024-08-15 20:02:26 -0400

    model trained

--- src/utils.py
+++ src/utils.py
@@ -5,11 +5,28 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_model(x_train, y_train, x_test, y_test, model):
+    try:
+        report = {}
+        for i in range(len(list(model))):
+            model1 = list(model.values())[i]
+            model1.fit(x_train, y_train)
+            y_train_pred = model1.predict(x_train)
+            y_test_pred = model1.predict(x_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(model.keys())[i]] = test_model_score
+
+            return report
     except Exception as e:
         raise CustomException(e,sys)
